3878f98173e86182b1fefe8a9ec40cd32c052daf
SullFurix <39709633+SullFurix@users.noreply.github.com>
2019-11-15 22:14:24 +0100

    1.0

--- OldWallet.bat
+++ OldWallet.bat
@@ -1,9 +1,11 @@
 @echo off & cls & color 0B
-Mode con cols=72 lines=12
+mode con cols=70 lines=13
 
 :menu
-Cls & color 0B
+cls & color 0B
+mode con cols=70 lines=13
 Title Old Wallet
+
 echo.
 echo.      ==========================Menu============================
 echo.
@@ -15,28 +17,97 @@ echo. & set /p choice=The number of your choice and press ENTER: || goto :menu
 echo. & call :menu_[%choice%]
 goto:menu
 ::********************************************************************************************
-:menu_[1] Add an address
-Add an address...
+:menu_[1] Add address
+mode con cols=70 lines=10
+Title Add address...
 cls & color 0B
 
-set /p cryptoName=Enter the name of the cryptocurrency you want to add:
-mkdir "./wallet/%cryptoName%"
+echo.
+echo.      ==========================Menu============================
+echo.
+echo.           [1]  Add an address and a cryptocurrency
+echo.           [2]  Add an address in an existing cryptocurrency
+echo.           [3]  Go back
+echo.
+echo.      ==========================================================
+set choice=
+echo. & set /p choice=The number of your choice and press ENTER: || goto :menu_[1]
+echo. & call :subAddMenu_[%choice%]
+goto:menu_[1]
+::=============================================
+:subAddMenu_[1]
+mode con cols=75 lines=13
+Title Add address and cryptocurrency...
+cls & color 0B
 
-set /p cryptoPublic=Enter the public key:
-echo.%cryptoPublic%>>./wallet/%cryptoName%/%cryptoName%_public.key
+set /p cryptoName=Enter the name of the cryptocurrency you wish to add(ex:Bitcoin):
+mkdir ".\wallet\%cryptoName%"
 
-set /p cryptoPrivate=Enter the private key:
-echo.%cryptoPrivate%>>./wallet/%cryptoName%/%cryptoName%_private.key
+set /p addressName=Enter the name of the address you wish to add(ex:Address1):
+
+set /p cryptoPublic=Enter your public key:
+echo.%addressName%(public):>>.\wallet\%cryptoName%\%cryptoName%_public.key
+echo.%cryptoPublic%>>.\wallet\%cryptoName%\%cryptoName%_public.key
+
+set /p cryptoPrivate=Enter your private key:
+echo.%addressName%(private):>>.\wallet\%cryptoName%\%cryptoName%_private.key
+echo.%cryptoPrivate%>>.\wallet\%cryptoName%\%cryptoName%_private.key
 
 echo Click on a key to return to the menu
 pause>nul
 goto:menu
-::********************************************************************************************
-:menu_[2] View a public key
-View a public key...
+::=============================================
+:subAddMenu_[2]
+mode con cols=90 lines=999999
+Title Add address
 cls & color 0B
 
-dir /b/o:n "./wallet/"
+dir /b/o:n ".\wallet\"
+
+set /p cryptoName=Choose the name of the cryptocurrency to which you want to add an address:
+set /p addressName=Enter the name of the address you wish to add(ex:Address1):
+
+set /p cryptoPublic=Enter your public key:
+echo.%addressName%(public):>>.\wallet\%cryptoName%\%cryptoName%_public.key
+echo.%cryptoPublic%>>.\wallet\%cryptoName%\%cryptoName%_public.key
+
+set /p cryptoPrivate=Enter your private key:
+echo.%addressName%(private):>>.\wallet\%cryptoName%\%cryptoName%_private.key
+echo.%cryptoPrivate%>>.\wallet\%cryptoName%\%cryptoName%_private.key
+
+echo Click on a key to return to the menu
+pause>nul
+goto:menu
+::=============================================
+:subAddMenu_[3]
+goto:menu
+::********************************************************************************************
+:menu_[2] Show address
+mode con cols=84 lines=13
+Title Show address...
+cls & color 0B
+
+echo.
+echo.      ================================Menu==================================
+echo.
+echo.           [1]  Display the public key for a specific cryptocurrency
+echo.           [2]  Display the private key for a specific cryptocurrency
+echo.           [3]  Display all public keys of all crypto currencies
+echo.           [4]  Display all private keys of all crypto currencies
+echo.           [5]  Go back
+echo.
+echo.      ======================================================================
+set choice=
+echo. & set /p choice=The number of your choice and press ENTER: || goto :menu_[2]
+echo. & call :subShowMenu_[%choice%]
+goto:menu_[2]
+::=============================================
+:subShowMenu_[1] Show a public key
+mode con cols=72 lines=999999
+Title Show a public key...
+cls & color 0B
+
+dir /b/o:n ".\wallet\"
 
 set /p cryptoName=Enter the name of the cryptocurrency:
 set publicKey=.\wallet\%cryptoName%\%cryptoName%_public.key
@@ -46,12 +117,13 @@ type %publicKey%
 echo Click on a key to return to the menu
 pause>nul
 goto:menu
-::********************************************************************************************
-:menu_[3] View a private key
-View a private key...
+::=============================================
+:subShowMenu_[2] Show a private key
+mode con cols=72 lines=999999
+Title Show a private key...
 cls & color 0B
 
-dir /b/o:n "./wallet/"
+dir /b/o:n ".\wallet\"
 
 set /p cryptoName=Enter the name of the cryptocurrency:
 set privateKey=.\wallet\%cryptoName%\%cryptoName%_private.key
@@ -61,23 +133,39 @@ type %privateKey%
 echo Click on a key to return to the menu
 pause>nul
 goto:menu
-::********************************************************************************************
-:menu_[4] View all public keys
-View all public keys...
+::=============================================
+:subShowMenu_[3] Show all public keys
+mode con cols=72 lines=999999
+Title Show all public keys...
 cls & color 0B
 
-for /f "tokens=*" %%c in ('dir /b/o:n "./wallet/"') do echo %%c:&& type ".\wallet\%%c\%%c_public.key"
+for /f "tokens=*" %%c in ('dir /b/o:n ".\wallet\"') do echo ----%%c----&& type ".\wallet\%%c\%%c_public.key"
 
 echo.
 echo Click on a key to return to the menu
 pause>nul
 goto:menu
-::********************************************************************************************
-:menu_[5] View all private keys
-View all private keys...
+::=============================================
+:subShowMenu_[4] Show all private keys
+mode con cols=72 lines=999999
+Title Show all private keys...
 cls & color 0B
 
-for /f "tokens=*" %%c in ('dir /b/o:n "./wallet/"') do echo %%c:&& type ".\wallet\%%c\%%c_private.key"
+for /f "tokens=*" %%c in ('dir /b/o:n ".\wallet\"') do echo ----%%c----&& type ".\wallet\%%c\%%c_private.key"
+
+echo.
+echo Click on a key to return to the menu
+pause>nul
+goto:menu
+::=============================================
+:subShowMenu_[5]
+goto:menu
+::********************************************************************************************
+:menu_[3] Delete an address
+Title Delete all private keys...
+cls & color 0B
+
+type ".\wallet\%cryptoName%\%cryptoName%_private.key" | findstr /b /v "%addressName%">%cryptoName%_private.key
 
 echo.
 echo Click on a key to return to the menu
@@ -86,3 +174,15 @@ goto:menu
 ::********************************************************************************************
 :EOF
 EXIT
+
+
+
+
+
+
+
+
+
+
+
+
